7737bfb8a2ef09c1f0f3ccd7099a811464d607b5
AcacieS <acaciesong@gmail.com>
2024-01-03 23:55:15 -0500

    obstacles added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,10 @@ public class PlayerController : MonoBehaviour
 {
     //Rigidbody call it of the unity
     private Rigidbody playerRb;
-    public float jumpForce = 10.0f
+    public float jumpForce = 15.0f;
+    //gravity set to 1 is the basic normal gravity
+    public float gravityModifier = 2;
+    public bool isOnGround = true;
 
     // Start is called before the first frame update
     void Start()
@@ -14,18 +17,26 @@ public class PlayerController : MonoBehaviour
         //GetComponent inside
         //<> is getting a type.
         playerRb = GetComponent<Rigidbody>();
-        //all the physics in unity
-        Physics.gravity *=
+        //all the physics in unity 
+        //Physics.gravity = Physics.gravity * gravityModifier: is *=
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
             //AddForce is like Translate
             //4 types of force mode. Normal: apply over time. Impulse: imediate apply 
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            //not longer be on ground.
+            isOnGround = false;
         }
+    }
+    //if player enter collision onto something
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
