aa6fa945deac13431f55fd4c1ce01d5f03a80615
AcacieS <acaciesong@gmail.com>
2024-01-08 18:49:32 -0500

    animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     //Rigidbody call it of the unity
     private Rigidbody playerRb;
+    private Animator playerAnim;
     public float jumpForce = 15.0f;
     //gravity set to 1 is the basic normal gravity
     public float gravityModifier = 2;
@@ -19,6 +20,7 @@ public class PlayerController : MonoBehaviour
         //GetComponent inside
         //<> is getting a type.
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         //all the physics in unity 
         //Physics.gravity = Physics.gravity * gravityModifier: is *=
         Physics.gravity *= gravityModifier;
@@ -27,13 +29,16 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        //!bool is False
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             //AddForce is like Translate
             //4 types of force mode. Normal: apply over time. Impulse: imediate apply 
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             //not longer be on ground.
             isOnGround = false;
+            //SetTrigger("name") as you can see in the name trigger. 
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
     //if player enter collision onto something
@@ -50,6 +55,9 @@ public class PlayerController : MonoBehaviour
         {
             gameOver = true;
             Debug.Log("Game Over");
+            //set bool set integer for the type ("name", what equal)
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
         }
     }
 }
